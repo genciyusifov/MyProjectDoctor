d7f872930e305b87d1a1df5539e0e9defd6fb78b
Gənci <yusifovgenci91@gmail.com>
2023-05-01 17:34:04 +0400

    Js kodu duzgun deyil bu kodu update edəcəm

--- js/script.js
+++ js/script.js
@@ -10,3 +10,21 @@ function toggleNavbar() {
 }
 
 hamburger.addEventListener("click", toggleNavbar);
+
+let soz1 = document.getElementById("soz1");
+let soz2 = document.getElementById("soz2");
+
+setInterval(function() {
+  if (soz1.style.opacity == "0") {
+    soz2.style.opacity = "1";
+    soz1.style.opacity = "0";
+  } else if (soz1.style.opacity !== "0") {
+    soz1.style.opacity = "1";
+    soz2.style.opacity = "0";
+  }
+}, 1000);
+
+
+
+
+
