33249af4d8ec4c8fa7b89bac1ce9cf57333f1882
Gənci <yusifovgenci91@gmail.com>
2023-05-02 13:49:03 +0400

    Problem həll olundu

--- js/script.js
+++ js/script.js
@@ -14,15 +14,36 @@ hamburger.addEventListener("click", toggleNavbar);
 let soz1 = document.getElementById("soz1");
 let soz2 = document.getElementById("soz2");
 
+
+soz1.style.opacity = "1";
+soz2.style.opacity = "0";
+
 setInterval(function() {
-  if (soz1.style.opacity == "0") {
+  if (soz1.style.opacity == 0) {
+    soz2.style.opacity = "0";
+    soz1.style.opacity = "1";
+  } else {
     soz2.style.opacity = "1";
     soz1.style.opacity = "0";
-  } else if (soz1.style.opacity !== "0") {
-    soz1.style.opacity = "1";
-    soz2.style.opacity = "0";
   }
-}, 2000);
+}, 4000);
+
+  // setInterval(function() {
+  //   if (soz1.style.opacity == 0) {
+  //     soz2.style.opacity = "0";
+  //   } else {
+  //     soz2.style.opacity = "1";
+  //   }
+  // }, 4000);
+
+  // setInterval(function() {
+  //   if (soz2.style.opacity == 1) {
+  //     soz1.style.opacity = "0";
+  //   } else {
+  //     soz1.style.opacity = "1";
+  //   }
+  // }, 4000);
+
 
 
 
